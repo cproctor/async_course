e44866879771f759d7ed664b2d58403edd0713c2
Chris Proctor <github.com@accounts.chrisproctor.net>
2022-08-22 15:51:42 -0400

    Cleaned up forms

--- async_course/assignments/forms.py
+++ async_course/assignments/forms.py
@@ -5,7 +5,7 @@ import magic
 class AssignmentForm(forms.ModelForm):
     class Meta:
         model = Assignment
-        fields = ['due_date', 'title', 'slug', 'active', 'peer_review', 'markdown']
+        fields = ['due_date', 'title', 'slug', 'markdown', 'due_date', 'active', 'peer_review']
 
 class SubmissionForm(forms.ModelForm):
     class Meta:
--- async_course/pages/forms.py
+++ async_course/pages/forms.py
@@ -4,4 +4,4 @@ from .models import Page
 class PageForm(forms.ModelForm):
     class Meta:
         model = Page
-        fields = ['in_menu', 'menu_label', 'slug', 'markdown']
+        fields = ['menu_label', 'slug', 'markdown', 'in_menu']
