12e8f209a49c8b5cf6c4e8522e01179db15f9721
Chris Proctor <github.com@accounts.chrisproctor.net>
2022-08-31 14:35:48 -0400

    Add email test functionality

--- async_course/common/urls.py
+++ async_course/common/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = "common"
 urlpatterns = [
     path('', views.HomeView.as_view(), name="home"),    
+    path('email/<int:index>', views.TestEmail.as_view(), name='test_email'),
 ]
 
--- async_course/common/views.py
+++ async_course/common/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views.generic.base import TemplateView
 from django.contrib.auth.forms import AuthenticationForm
+from django.conf import settings
+from common.email import send_journal_email
 
 class HomeView(TemplateView):
     template_name = "common/home.html"
@@ -13,3 +15,20 @@ class HomeView(TemplateView):
                 'form': AuthenticationForm,
             }
             return render(request, self.template_name, context)
+
+class TestEmail(TemplateView):
+    template_name = "common/email_test.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['index'] = self.kwargs['index']
+        return context
+
+    def get(self, *args, **kwargs):
+        index = self.kwargs['index']
+        send_journal_email(
+            f"Email test {index}",
+            f"This is a test.",
+            [addr for name, addr in settings.ADMINS]
+        )
+        return super().get(*args, **kwargs)
