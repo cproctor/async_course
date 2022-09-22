f32ca3598d432912c8a84d27fb15e4a8010700f0
Chris Proctor <github.com@accounts.chrisproctor.net>
2022-09-22 06:46:45 -0400

    Update reviews templatetag to accurately show status

--- async_course/reviews/templatetags/review_tags.py
+++ async_course/reviews/templatetags/review_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from reviews.models import ReviewerRole
+from reviews.models import ReviewerRole, Review
 from assignments.models import Assignment
 from events.models import Event, Notification
 
@@ -15,10 +15,12 @@ def reviews_needing_action_count(user):
 def assignments_with_unseen_reviews(user):
     if not user.is_authenticated or user.profile.is_teacher:
         return 0
+    review_ids = [r.id for r in Review.objects.filter(submission__author=user)]
     notifications = Notification.objects.filter(
         user=user,
         read=False,
         event__action=Event.EventActions.ADDED_REVIEW,
+        event__object_id__in=review_ids
     ).select_related('event')
     review_ids = set([n.event.object_id for n in notifications])
     assns =  Assignment.objects.filter(reviewer_roles__reviews__in=review_ids).distinct().count()
