a4caf8091ebb684d1c11dc2ef204f2bf086cb410
Chris Proctor <github.com@accounts.chrisproctor.net>
2022-12-11 16:48:23 -0500

    Add verbose option to posts report

--- async_course/posts/management/commands/posts_report.py
+++ async_course/posts/management/commands/posts_report.py
@@ -6,14 +6,14 @@ from posts.models import Post
 class Command(BaseCommand):
     help="Reset the ancestor/descendent relations for all posts, based on parent."
 
-    def handle(self, *args, **kwargs):
+    def handle(self, *args, **options):
         counts, points = [], []
-        for user in User.objects.filter(profile__is_teacher=False).all():
+        for user in User.objects.filter(profile__is_student=True):
             counts.append(user.posts.count())
             points.append(sum([p.score() for p in user.posts.all()]))
         print(f"Post count: mean {round(mean(counts), 1)}, median {round(median(counts), 1)}, stdev {round(stdev(counts), 1)}")
         print(f"Total points: mean {round(mean(points), 1)}, median {round(median(points), 1)}, stdev {round(stdev(points), 1)}")
-                
-
-        
-
+        if options['verbosity'] > 0:
+            scores = [(sum([p.score() for p in u.posts.all()]), u.username) for u in User.objects.filter(profile__is_student=True)]
+            for score, username in sorted(scores, reverse=True):
+                print(f" - {username}: {score} points")
